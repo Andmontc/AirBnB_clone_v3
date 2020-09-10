a904aae92c20cc43bce3d74ea8d9bedc0c954430
Andmontc <oscarandresmontes@gmail.com>
2020-09-10 00:16:55 +0000

    [Update] add places file

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,6 +5,7 @@ from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.user import User
+from models.place import Place
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
@@ -13,3 +14,4 @@ from api.v1.views.states import *
 from api.v1.views.cities import *
 from api.v1.views.amenities import *
 from api.v1.views.users import *
+from api.v1.views.places import *
